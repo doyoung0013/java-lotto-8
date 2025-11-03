c8bd2fd9bf091a214479b2af6b339aaf22f68dc0
kdy <sksl6134@gmail.com>
2025-11-03 20:55:41 +0900

    feat: 사용자로부터 구입 금액 입력 받기 및 예외 처리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,12 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class LottoController {
+	
+	public void run() {
+		int purchaseAmount = InputView.inputPurchaseAmount();
+		
+	}
 
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,31 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+	
+	public static int inputPurchaseAmount() {
+		
+		System.out.println("구입금액을 입력해 주세요.");
+		String inputLine = Console.readLine();
+		int amount = validatePurchaseAmount(inputLine);
+        return amount;
+        
+	}
+	
+	public static int validatePurchaseAmount(String inputLine){
+		try {
+            int amount = Integer.parseInt(inputLine.trim());
+            if (amount <= 0) {
+                throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해 주세요.");
+            }
+            if (amount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+            }
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 정확히 입력해 주세요.");
+        }
+	}
 
 }
